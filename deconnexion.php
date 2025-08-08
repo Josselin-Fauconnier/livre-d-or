4403230a894d12f51f56e64d4d50c050e9396cf2
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-08 16:31:51 +0200

    first commit

---
+++ deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if (session_status()===PHP_SESSION_ACTIVE){
+    $_SESSION=array();
+    if (isset($_COOKIE[session_name()])){
+        setcookie(session_name(),'',time()-5400,'/');
+}
+    session_destroy();
+}
+
+ header("Location: index.php");
+  exit();
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Présentation du livre d'or</title>
+    <link rel="stylesheet" href="style_or.css">
+</head>
+<body>
+<header class="contient_head">
+</header>
+<main>
+        <
+</main>
+</body>
+</html>
