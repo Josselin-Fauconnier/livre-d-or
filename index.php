6ff8e2e6153b13aeb35e18b16bbe9cee9eba5097
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-15 17:58:17 +0200

    travail de la semaine

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<php
+<?php
 session_start();
+date_default_timezone_set("Europe/Paris");
 
+$actual_date=date('j F,d,m,Y-H:i:s');
+
+$date_FR= new IntlDateFormatter (
+    'fr_FR',
+    IntlDateFormatter::FULL,
+    IntlDateFormatter::FULL,
+    "Europe/Paris",
+    IntlDateFormatter::GREGORIAN
+);
+
+$date_message='Nous sommes le' . $date_FR;
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,10 +24,21 @@ session_start();
     <link rel="stylesheet" href="style_or.css">
 </head>
 <body>
-<header class="contient_head">
+<header class="en-tête">
+    <p> <?php
+        if(isset($_SESSION['user'])){
+            echo"Bienvenue,". htmlspecialchars($_SESSION['user']['login']);
+        }else{
+            echo"Bienvenue invité";
+        }
+            ?>
+    </p>
+    <p>
+      <?php  echo $date_message ?>
+    </p>
 </header>
 <main>
-        <
+        
 </main>
 </body>
 </html>
