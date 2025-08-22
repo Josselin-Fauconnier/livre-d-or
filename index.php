b3cc582e9b7bfe39640457fe7261303ef1f15eb4
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-22 18:22:27 +0200

    chanchement

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 session_start();
 date_default_timezone_set("Europe/Paris");
 
-$actual_date=date('j F,d,m,Y-H:i:s');
-
 $date_FR= new IntlDateFormatter (
     'fr_FR',
     IntlDateFormatter::FULL,
     IntlDateFormatter::FULL,
     "Europe/Paris",
-    IntlDateFormatter::GREGORIAN
+    IntlDateFormatter::GREGORIAN,
+    "EEEE d MMMM y HH:mm"
 );
 
-$date_message='Nous sommes le' . $date_FR;
+
+$format_date= new DateTime();
 ?>
 
 
@@ -24,8 +24,9 @@ $date_message='Nous sommes le' . $date_FR;
     <link rel="stylesheet" href="style_or.css">
 </head>
 <body>
-<header class="en-tête">
-    <p> <?php
+<header >
+   <div>
+        <p> <?php
         if(isset($_SESSION['user'])){
             echo"Bienvenue,". htmlspecialchars($_SESSION['user']['login']);
         }else{
@@ -34,11 +35,31 @@ $date_message='Nous sommes le' . $date_FR;
             ?>
     </p>
     <p>
-      <?php  echo $date_message ?>
+      <?php  echo "Nous sommes le " .$date_FR->format($format_date)  ?>
     </p>
+   </div> 
 </header>
 <main>
-        
+    <nav id="nav-bar">
+        <?php if (isset($_SESSION['user'])):?>
+            <a href="index.php">L'index</a>
+            <a href="profil.php">Mon profil</a>
+            <a href="livre-or.php">Le livre</a>
+            <a href="commentaire.php">Faire un commentaire</a>
+            <a href="deconnexion.php">Se déconnecter</a>
+        <?php else: ?>
+            <a href="index.php">L'index</a>
+            <a href="inscription.php">S'inscrire</a>
+            <a href="connexion.php">Se connecter</a>
+            <a href="livre-or.php">Le livre</a>
+        <?php endif; ?>
+    </nav>
+   <div class="conteneur">
+        <article class="article_presentation">
+            <h1>Présentation du site</h1>
+            <p> Ce site sert à recuillier divers retour sur les projets pister sur ce githubs</p>
+        </article>
+   </div>
 </main>
 </body>
 </html>
